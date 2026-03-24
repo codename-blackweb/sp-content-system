5b9a1cd98749c1afffd1ef5af5f802c6e93c8405
blackezrah <black.ezra.h@gmail.com>
2026-03-24 12:59:19 -0400

    Fix mobile cloud repetition illusion...again

--- script.js
+++ script.js
@@ -153,8 +153,8 @@ function updateScene() {
     root.style.setProperty("--stars-opacity", "0");
 
     if (cloudTopLayer && cloudBottomLayer) {
-      cloudTopLayer.style.transform = "translateX(-50%)";
-      cloudBottomLayer.style.transform = "translateX(-50%) rotate(180deg)";
+      cloudTopLayer.style.transform = "";
+      cloudBottomLayer.style.transform = "";
       cloudTopLayer.style.filter = "none";
       cloudBottomLayer.style.filter = "none";
     }
